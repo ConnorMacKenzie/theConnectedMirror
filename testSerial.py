739c654e6044c465cc2046bb69e2210bd9ff8b81
Zein Hajj-Ali <zeinhajjali@cmail.carleton.ca>
2017-11-15 17:43:32 -0500

    Uncommented hdmi toggles

--- testSerial.py
+++ testSerial.py
@@ -6,22 +6,21 @@ onOff = 1;
 
 while 1:
 
-        if int(ser.readline())<75:
-                print("Close");
-                print(ser.readline());
-                if onOff == 0:
-                        #os.system("tvservice -o");
-                        ser.write('1');
-                        onOff = 1;
+    if int(ser.readline())<75:
+        print("Close");
+        print(ser.readline());
+        if onOff == 0:
+            os.system("tvservice -o");
+            ser.write('1');
+            onOff = 1;
 
-        elif int(ser.readline())>=75:
-                print("Far");
-                print(ser.readline());
-                if onOff == 1:
-                        #os.system("tvservice -p");
-                        #os.system("xset -display :0 dpms force on");
-                        ser.write('0');
-                        onOff = 0;
-        else:
-                pass;
-
+    elif int(ser.readline())>=75:
+        print("Far");
+        print(ser.readline());
+        if onOff == 1:
+            os.system("tvservice -p");
+            os.system("xset -display :0 dpms force on");
+            ser.write('0');
+            onOff = 0;
+    else:
+        pass;
