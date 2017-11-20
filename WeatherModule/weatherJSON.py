ce86a8af3b54ebfae6eacafca6d38dcd1c3b7d25
Zein Hajj-Ali <zedzein@gmail.com>
2017-11-20 12:26:06 -0600

    Organised in folders

---
+++ WeatherModule/weatherJSON.py
@@ -0,0 +1,13 @@
+import forecastio
+
+apikey = "566169fcb4282ff1c9d716df2ccdfd90"
+lat = 45.411572
+lng = -75.698194
+forecast = forecastio.load_forecast(apikey, lat, lng)
+
+curr = forecast.currently()
+
+print ("It is currently " + curr.summary)
+print ("At a temperature of " + str(curr.temperature) + " with a " + str(curr.humidity*100) + " percent humidity")
+if curr.precipProbability > 0:
+    print ("There is a " + str(curr.precipProbability*100) + " percent chance of " + curr.precipType)
