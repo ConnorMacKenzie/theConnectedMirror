520e0071c3efab59a620d0db7c5c8716dea55896
Zein Hajj-Ali <zedzein@gmail.com>
2017-11-20 13:23:50 -0600

    started JSON encoding

--- WeatherModule/weatherJSON.py
+++ WeatherModule/weatherJSON.py
@@ -1,4 +1,6 @@
 import forecastio
+import json
+import StringIO
 
 apikey = "566169fcb4282ff1c9d716df2ccdfd90"
 lat = 45.411572
@@ -7,7 +9,11 @@ forecast = forecastio.load_forecast(apikey, lat, lng)
 
 curr = forecast.currently()
 
-print ("It is currently " + curr.summary)
+summ = json.dumps(["It is currently " + curr.summary])
+
+summ = summ +
+
+summ = summ + json.dumps(["It is currently " + curr.summary])
 print ("At a temperature of " + str(curr.temperature) + " with a " + str(curr.humidity*100) + " percent humidity")
 if curr.precipProbability > 0:
     print ("There is a " + str(curr.precipProbability*100) + " percent chance of " + curr.precipType)
