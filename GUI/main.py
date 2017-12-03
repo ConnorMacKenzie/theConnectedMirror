49685d49eb587cac01385979a47d2ec7028fb4ff
Connor MacKenzie <connormack97@gmail.com>
2017-12-03 22:08:05 +0000

    gui test

--- GUI/main.py
+++ GUI/main.py
@@ -6,24 +6,25 @@ import serial
 class MainClient():
 
     ser = serial.Serial('/dev/ttyACM0', 9600)
-    onOff = 1;
-
-
-
-    if int(ser.readline())<75:
-        if onOff == 0:
-            os.system("tvservice -p");
-            os.system("xset -display :0 dpms force on");
-            ser.write('1');
-            onOff = 1;
-
-    elif int(ser.readline())>=75:
-        if onOff == 1:
-            os.system("tvservice -o");
-            ser.write('0');
-            onOff = 0;
-    else:
-        pass;
+    print ser.readline()
+##    onOff = 1;
+##
+##
+##
+##    if int(ser.readline())<75:
+##        if onOff == 0:
+##            os.system("tvservice -p");
+##            os.system("xset -display :0 dpms force on");
+##            ser.write('1');
+##            onOff = 1;
+##
+##    elif int(ser.readline())>=75:
+##        if onOff == 1:
+##            os.system("tvservice -o");
+##            ser.write('0');
+##            onOff = 0;
+##    else:
+##        pass;
 
         
 Process(target= GUIClient.GUI()).start()
