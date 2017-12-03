2818e6dfefb614575fa8f46f43df4e85088fac85
Theo Hronowsky <theohronowsky@LABS.SCE.CARLETON.CA>
2017-12-03 16:30:26 -0500

    main files

---
+++ GUI/main.py
@@ -0,0 +1,46 @@
+import json, socket, time, sys, GUIClient, speechMain, Tkinter
+from multiprocessing import Process
+#import serial
+
+
+class MainClient():
+
+    #ser = serial.Serial('/dev/ttyACM0', 9600)
+    onOff = 1;
+
+
+    while 1:
+
+        if int(ser.readline())<75:
+            if onOff == 0:
+                os.system("tvservice -p");
+                os.system("xset -display :0 dpms force on");
+                ser.write('1');
+                onOff = 1;
+
+        elif int(ser.readline())>=75:
+            if onOff == 1:
+                os.system("tvservice -o");
+                ser.write('0');
+                onOff = 0;
+        else:
+            pass;
+
+        
+    while 1:       
+        Process(target= MainClient()).start()
+        Process(target= GUIClient.GUI()).start()
+   
+
+
+
+##
+##speech = speechMain.SpeechClient()
+##
+##command = speechMain.SpeachClient.recieveData();
+##
+##if command is 'news on':
+##    
+
+    
+
---
+++ GUI/speechMain.py
@@ -0,0 +1,38 @@
+
+
+class SpeechClient():
+
+    def __init__(self, local = 'localhost', localport = 22, remote = 'localhost', remoteport = 23):
+
+        #initializes socket addresses and ports
+        self.localName = local
+        self.localPort = localport
+        self.remoteName = remote
+        self.remotePort = remoteport
+
+        #initializes sockets
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.localAddress = (self.localName, self.localPort)
+        self.remoteAddress = (self.remoteName, self.remotePort)
+        self.s.bind(self.localAddress)
+
+   def recieveData(self, field):
+        #print ("Enter non-null weather pull request: ")
+        #sets the string to be sent as the next line in the terminal
+
+        #sends the next line in the terminal to the server socket
+        self.s.sendto(field.encode('utf-8'), self.remoteAddress)
+
+        #receives data from server and stores in buf
+        buf, address = self.s.recvfrom(100000)
+        #decodes utf-8 data to store json string
+        data = buf.decode('utf-8')
+
+        
+
+        return data
+
+        
+            
+
+    
