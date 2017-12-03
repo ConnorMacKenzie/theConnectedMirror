eb22e7c5a275a0ec91450557d6d7a5ad7d489152
Connor MacKenzie <connormack97@gmail.com>
2017-12-03 23:52:40 +0000

    delay on screen power off

--- GUI/main.py
+++ GUI/main.py
@@ -2,39 +2,45 @@ import json, socket, time, sys, speechMain, os
 from timeit import default_timer
 from multiprocessing import Process
 import serial
-from threading import Timer
+from time
 
 
 class MainClient():
 
-	ser = serial.Serial('/dev/ttyACM0', 9600)
+        ser = serial.Serial('/dev/ttyACM0', 9600)
 
-	start = None
+        start = None
+        end = None
+        isTimer = False
+        onOff = 1;
 
-	onOff = 1;
 
+        while 1:
+                if int(ser.readline())<75:
+                        if onOff == 0:
+                                os.system("tvservice -p");
+                                os.system("xset -display :0 dpms force on");
+                                ser.write('1');
+                                onOff = 1;
 
-	while 1:
-        	if int(ser.readline())<75:
-            		if onOff == 0:
-                		os.system("tvservice -p");
-                		os.system("xset -display :0 dpms force on");
-                		ser.write('1');
-                		onOff = 1;
+                elif int(ser.readline())>=75:
+                        if !isTimer:
+                                start = time.time()
+                                isTimer = True
 
-        	elif int(ser.readline())>=75:
-			t = Timer(5.0, lcdOff)
-            			
-        	else:
-            		pass;
+                        if isTimer:
+                                elapsed = time.time() - start
+                                if elapsed >= 5 and onOff == 1:
+                                        os.system("tvservice -o");
+                                        ser.write('0');
+                                        onOff = 0;
+                                        start = None
+                                        elapsed = None
+                                        isTimer = False
+                else:
+                        pass;
 
-        def lcdOff(self):
-                if int(ser.readline())>=75:
-                        if onOff == 1:
-                                os.system("tvservice -o");
-                                ser.write('0');
-                                onOff = 0;
-                                start = None
+                        
             
 ##    onOff = 1;
 ##
