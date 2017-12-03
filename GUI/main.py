dccd96475ab0f68d59d2d77e06e8c623af43ffc9
Theo Hronowsky <theohronowsky@LABS.SCE.CARLETON.CA>
2017-12-03 17:34:23 -0500

    main

--- GUI/main.py
+++ GUI/main.py
@@ -6,6 +6,27 @@ import serial
 class MainClient():
 
     ser = serial.Serial('/dev/ttyACM0', 9600)
+<<<<<<< HEAD
+    onOff = 1;
+
+
+    while 1:
+        if int(ser.readline())<75:
+            if onOff == 0:
+                os.system("tvservice -p");
+                os.system("xset -display :0 dpms force on");
+                ser.write('1');
+                onOff = 1;
+
+        elif int(ser.readline())>=75:
+            if onOff == 1:
+                os.system("tvservice -o");
+                ser.write('0');
+                onOff = 0;
+        else:
+            pass;
+
+=======
     print ser.readline()
 ##    onOff = 1;
 ##
@@ -30,18 +51,9 @@ class MainClient():
 Process(target= GUIClient.GUI()).start()
 while 1:       
         Process(target= MainClient()).start()
+>>>>>>> 49685d49eb587cac01385979a47d2ec7028fb4ff
         
-   
-
-
-
-##
-##speech = speechMain.SpeechClient()
-##
-##command = speechMain.SpeachClient.recieveData();
-##
-##if command is 'news on':
-##    
+main = MainClient()   
 
     
 
