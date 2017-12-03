d972df91bb02c1ecb8ebc1132c19c71c6f70e9c5
Zein Hajj Ali <zedzein@gmail.com>
2017-12-03 22:46:28 +0000

    changes

--- GUI/main.py
+++ GUI/main.py
@@ -5,26 +5,26 @@ import serial
 
 class MainClient():
 
-    ser = serial.Serial('/dev/ttyACM0', 9600)
+	ser = serial.Serial('/dev/ttyACM0', 9600)
 
-    onOff = 1;
+	onOff = 1;
 
 
-    while 1:
-        if int(ser.readline())<75:
-            if onOff == 0:
-                os.system("tvservice -p");
-                os.system("xset -display :0 dpms force on");
-                ser.write('1');
-                onOff = 1;
+	while 1:
+        	if int(ser.readline())<75:
+            		if onOff == 0:
+                		os.system("tvservice -p");
+                		os.system("xset -display :0 dpms force on");
+                		ser.write('1');
+                		onOff = 1;
 
-        elif int(ser.readline())>=75:
-            if onOff == 1:
-                os.system("tvservice -o");
-                ser.write('0');
-                onOff = 0;
-        else:
-            pass;
+        	elif int(ser.readline())>=75:
+            		if onOff == 1:
+                		os.system("tvservice -o");
+                		ser.write('0');
+                		onOff = 0;
+        	else:
+            		pass;
 
 
     
