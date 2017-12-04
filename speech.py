5551cc4bdf604d27592a48fb45f510bbc969ec68
Connor MacKenzie <connormack97@gmail.com>
2017-12-04 00:35:36 +0000

    Voice Command Sockets

--- speech.py
+++ speech.py
@@ -8,7 +8,7 @@ class Speech():
     @staticmethod
     def record():
         r = sr.Recognizer()
-        r.energy_threshhold = 2000
+        r.energy_threshhold = 3000
         with sr.Microphone() as source:
             print 'say something'
             try:
